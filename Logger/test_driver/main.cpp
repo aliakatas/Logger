447443ae979e530001ffabc1eea37e13fae90f1d
Aristotelis.Liakatas <Aristotelis.Liakatas@jacobs.com>
2021-09-17 15:29:10 +0100

    Removes OOP approach as not suitable (slow).

--- Logger/test_driver/main.cpp
+++ Logger/test_driver/main.cpp
@@ -1,12 +1,10 @@
 #include "Timer.h"
 #include "Logger.h"
-#include "LoggerStream.h"
 #include "text_definitions.h"
 #include <stdio.h>
 
 #define N				100000
 #define EXAMPLE_LOG		"classic_log.log"
-#define EXAMPLE_STREAM_LOG		"stream_log.log"
 
 int main(int argc, char** argv) {
 	fprintf_s(stderr, "Starting Logger test driver...\n");
@@ -43,39 +41,6 @@ int main(int argc, char** argv) {
 
 	my_classic_logger.close_file();
 	fprintf_s(stderr, "It took %.4f s to complete with C logger\n", my_timer.elapsed());
-
-	//************************
-
-	LoggerStream my_stream_logger;
-
-	my_timer.reset();
-	my_stream_logger.set_filename(EXAMPLE_STREAM_LOG);
-	my_stream_logger.open_file();
-
-	for (auto i = 0; i < N; ++i) {
-		if (!my_stream_logger.write("*****************"))
-			return -1;
-		if (!my_stream_logger.write(LINE1_TXT))
-			return -1;
-		if (!my_stream_logger.write(LINE2_TXT))
-			return -1;
-		if (!my_stream_logger.write(LINE3_TXT))
-			return -1;
-		if (!my_stream_logger.write(LINE4_TXT))
-			return -1;
-		if (!my_stream_logger.write(COMMON_FLOAT_PI))
-			return -1;
-		if (!my_stream_logger.write(COMMON_FLOAT_sq2))
-			return -1;
-		if (!my_stream_logger.write(2021, 9, 21))
-			return -1;
-		if (!my_stream_logger.write("*****************"))
-			return -1;
-	}
-
-	my_stream_logger.close_file();
-	fprintf_s(stderr, "It took %.4f s to complete with C++ logger\n", my_timer.elapsed());
-
 
 	fprintf_s(stderr, "\nDone\n");
 	return 0;
